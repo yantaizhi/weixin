56b3e3d8fb54e35969f6f226aa4223817328371a
砚台纸 <15000369318@163.com>
2019-12-05 16:08:47 +0800

    显示所在地天气

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,40 +7,65 @@ Page({
   data: {
     region:['北京市','北京市','东城区'],
     now:{},
-    province:{}
+    province:'',
+    city:'',
+    locat:''
   },
   changeRegion:function(e){
     this.setData({
       region:e.detail.value
     })
-    this.getWeather()
+    this.change()
+  },
+  change:function(){
+    var that = this;
+    wx.request({
+      url:'https://free-api.heweather.net/s6/weather/now?',
+      data:{
+        location:that.data.region[1],
+        key:'d4f71f98a6bd4b5a89849027a3030a1d'
+      },
+      success:function(res){
+        that.setData({
+          now: res.data.HeWeather6[0].now,
+        }),
+        console.log(res.data)
+      }
+    }) 
   },
   getWeather:function(){
     var that = this;
     wx.request({
       url: 'https://free-api.heweather.net/s6/weather/now?',
       data:{
-        location:that.data.region[1],
+        location:that.data.locat,
         key:'d4f71f98a6bd4b5a89849027a3030a1d'
       },
       success:function(res){
         console.log(res.data)
         that.setData({
-          now: res.data.HeWeather6[0].now
+          now: res.data.HeWeather6[0].now,
+          province: res.data.HeWeather6[0].basic.admin_area,
+          city: res.data.HeWeather6[0].basic.parent_city,
+          region: [res.data.HeWeather6[0].basic.admin_area, res.data.HeWeather6[0].basic.parent_city, res.data.HeWeather6[0].basic.location]
         })
-      }
+      } 
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
     wx.getLocation({
       success: function(res) {
         console.log(res)
+        var locat = res.latitude.toString() +','+ res.longitude.toString()
+        console.log(locat)
+        that.data.locat = locat
+        that.getWeather() 
       },
     })
-    this.getWeather()
   },
 
   /**
