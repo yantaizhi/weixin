09037fb746a978f9fc3c7ea5de7fcc5691191b4d
砚台纸 <15000369318@163.com>
2019-12-05 14:59:44 +0800

    可显示当前天气

---
+++ pages/index/index.js
@@ -0,0 +1,94 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    region:['北京市','北京市','东城区'],
+    now:{},
+    province:{}
+  },
+  changeRegion:function(e){
+    this.setData({
+      region:e.detail.value
+    })
+    this.getWeather()
+  },
+  getWeather:function(){
+    var that = this;
+    wx.request({
+      url: 'https://free-api.heweather.net/s6/weather/now?',
+      data:{
+        location:that.data.region[1],
+        key:'d4f71f98a6bd4b5a89849027a3030a1d'
+      },
+      success:function(res){
+        console.log(res.data)
+        that.setData({
+          now: res.data.HeWeather6[0].now
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getLocation({
+      success: function(res) {
+        console.log(res)
+      },
+    })
+    this.getWeather()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
